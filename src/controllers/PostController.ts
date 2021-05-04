6ff78d5be3835eb6c68e2b8a0d934cf71c90135a
Nghia Phung <nghia.phung@earthtoday.com>
2021-05-04 10:36:13 +0700

    feat: add eslint

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -13,7 +13,7 @@ import { AppRequest } from "../middlewares/BootstrapRouter";
 @JsonController()
 export class PostController {
   @Get("/v1/post/category")
-  async getListCategory(@Req() req: AppRequest) {
+  async getListCategory(@Req() req: AppRequest): Promise<any> {
     return req.interactor.getCategoryInteractor.execute(req);
   }
   @Post("/v1/post/create")
@@ -21,7 +21,7 @@ export class PostController {
     @Req() req: AppRequest,
     @UploadedFile("photo") photo: Express.Multer.File,
     @Body() payload: CreatePostPayload
-  ) {
+  ): Promise<any> {
     return req.interactor.createPostInteractor.execute(req, photo, payload);
   }
 }
